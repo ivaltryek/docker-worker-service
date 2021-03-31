0a151abf9f97220324edbd23ae7d62072d0aa8b1
Meet Vasani <meet.vasani86@gmail.com>
2021-03-31 20:08:04 +0530

    End Commit

---
+++ MyJobScheduler.cs
@@ -0,0 +1,18 @@
+using Coravel.Invocable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace docker_worker_service
+{
+    public class MyJobScheduler : IInvocable
+    {
+        public Task Invoke()
+        {
+            File.AppendAllText("logs.txt", "This is called at: " + DateTime.Now.ToString() + Environment.NewLine);
+            return Task.CompletedTask;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,23 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Coravel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 namespace docker_worker_service
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            host.Services.UseScheduler(sch =>
+            {
+                sch.Schedule<MyJobScheduler>().EveryFiveSeconds();
+            });
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+                .ConfigureServices(services =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.AddScheduler();
+                    services.AddTransient<MyJobScheduler>();
                 });
     }
 }
